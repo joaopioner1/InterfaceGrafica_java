22eb27a2c5f3739b1738195acfd17550740904c5
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-11-06 16:13:28 -0300

    Adc programa para calcular a media atualizado

--- school/MainClass.java
+++ school/MainClass.java
@@ -6,7 +6,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import view.JustNumbers;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -19,7 +18,7 @@ public class MainClass extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	// AUTOR: JOAO VITOR SOUZA PIONER | DATA: 6/11/2020 8:26
+	// AUTOR: JOAO VITOR SOUZA PIONER | DATA: 6/11/2020 16:09
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -42,12 +41,12 @@ public class MainClass extends JFrame {
 	public JTextField txtInputAverage;
 	public JLabel lblShowAvg;
 	public JTextField txtMedia;
-	public JLabel lblSitucaoDoAluno;
 	public JTextField txtSituacao;
-	private JLabel lblMedia;
-	private JLabel lblSituoDoAluno;
-
+	public JLabel lblSituoDoAluno;
+	public JLabel lblMedia;
+	
 	public MainClass() {
+		setResizable(false);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 369, 311);
@@ -60,30 +59,33 @@ public class MainClass extends JFrame {
 
 		// TXTBOXS
 		txtNumber1 = new JTextField();
-		txtNumber1.setFont(new Font("Arial", Font.PLAIN, 12));
+		txtNumber1.setFont(new Font("Arial", Font.PLAIN, 13));
 		txtNumber1.setColumns(10);
 		txtNumber1.setBounds(10, 79, 96, 23);
 		pane1.add(txtNumber1);
-		txtNumber1.setDocument(new JustNumbers());
 
 		txtNumber2 = new JTextField();
-		txtNumber2.setFont(new Font("Arial", Font.PLAIN, 12));
+		txtNumber2.setFont(new Font("Arial", Font.PLAIN, 13));
 		txtNumber2.setColumns(10);
 		txtNumber2.setBounds(124, 79, 96, 23);
 		pane1.add(txtNumber2);
-		txtNumber2.setDocument(new JustNumbers());
 
 		txtNumber3 = new JTextField();
-		txtNumber3.setFont(new Font("Arial", Font.PLAIN, 12));
+		txtNumber3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				sC.ShowData(); // Metodo que executa o codigo
+			}
+		});
+		txtNumber3.setFont(new Font("Arial", Font.PLAIN, 13));
 		txtNumber3.setColumns(10);
 		txtNumber3.setBounds(239, 79, 96, 23);
 		pane1.add(txtNumber3);
-		txtNumber3.setDocument(new JustNumbers());
 
 		txtInputAverage = new JTextField();
-		txtInputAverage.setFont(new Font("Arial", Font.PLAIN, 12));
+		txtInputAverage.setFont(new Font("Arial", Font.PLAIN, 13));
 		txtInputAverage.setColumns(10);
-		txtInputAverage.setBounds(10, 24, 106, 23);
+		txtInputAverage.setBounds(10, 24, 96, 23);
 		pane1.add(txtInputAverage);
 		
 		// LABELS
@@ -100,48 +102,45 @@ public class MainClass extends JFrame {
 		pane1.add(lblAvarege);
 		
 		lblMedia = new JLabel("M\u00E9dia do aluno:");
-		lblMedia.setForeground(new Color(0, 0, 128));
+		lblMedia.setForeground(new Color(173, 255, 47));
 		lblMedia.setFont(new Font("Arial", Font.BOLD, 15));
 		lblMedia.setBounds(10, 139, 125, 23);
 		pane1.add(lblMedia);
 		
 		lblSituoDoAluno = new JLabel("Situ\u00E7\u00E3o do aluno:");
-		lblSituoDoAluno.setForeground(new Color(0, 0, 128));
+		lblSituoDoAluno.setForeground(new Color(173, 255, 47));
 		lblSituoDoAluno.setFont(new Font("Arial", Font.BOLD, 15));
 		lblSituoDoAluno.setBounds(10, 173, 125, 23);
 		pane1.add(lblSituoDoAluno);
 		
 		// BOTTON
-		JButton btnCalculate = new JButton("Calcular");
-		btnCalculate.setForeground(new Color(255, 255, 255));
-		btnCalculate.setBackground(new Color(112, 128, 144));
-		btnCalculate.setFont(new Font("Arial", Font.BOLD, 14));
-		btnCalculate.setBounds(124, 230, 96, 31);
-		pane1.add(btnCalculate);
-		
-		// METODO BUTTON
-		btnCalculate.addActionListener(new ActionListener() {
+		JButton btnApagar = new JButton("Apagar ");
+		btnApagar.addActionListener(new ActionListener() {
+			// METODO BUTTON
 			public void actionPerformed(ActionEvent e) {
 				
-				txtMedia = new JTextField();
-				txtMedia.setEditable(false);
-				txtMedia.setFont(new Font("Arial", Font.PLAIN, 15));
-				txtMedia.setForeground(new Color(0, 0, 128));
-				txtMedia.setBounds(124, 139, 211, 23);
-				pane1.add(txtMedia);
-				txtMedia.setColumns(10);
-
-				txtSituacao = new JTextField();
-				txtSituacao.setEditable(false);
-				txtSituacao.setForeground(new Color(0, 0, 128));
-				txtSituacao.setFont(new Font("Arial", Font.PLAIN, 15));
-				txtSituacao.setColumns(10);
-				txtSituacao.setBounds(134, 173, 201, 23);
-				pane1.add(txtSituacao);
+				sC.CleanCache();
+			}
+		});
+		btnApagar.setForeground(Color.WHITE);
+		btnApagar.setFont(new Font("Arial", Font.BOLD, 14));
+		btnApagar.setBackground(new Color(112, 128, 144));
+		btnApagar.setBounds(187, 230, 96, 31);
+		pane1.add(btnApagar);
+	
+		JButton btnCalculate = new JButton("Calcular");
+		btnCalculate.addActionListener(new ActionListener() {
+			// METODO BUTTON
+			public void actionPerformed(ActionEvent e) {
 				
 				sC.ShowData(); // Metodo que executa o codigo
 			}
 		});
-
+		btnCalculate.setForeground(new Color(255, 255, 255));
+		btnCalculate.setBackground(new Color(112, 128, 144));
+		btnCalculate.setFont(new Font("Arial", Font.BOLD, 14));
+		btnCalculate.setBounds(64, 230, 96, 31);
+		pane1.add(btnCalculate);
+		
 	}
 }
--- school/SecundaryClass.java
+++ school/SecundaryClass.java
@@ -1,31 +1,61 @@
 package school;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JTextField;
+
 public class SecundaryClass {
-	
+
 	final MainClass mN;
-	
-	 public SecundaryClass(MainClass mn) { // recebe a tela como parâmetro ao invés de instanciar novamente
-	        this.mN = mn;
-	    }
-	
-	 public double CalculaMedia() {
-		
-		double a = Double.parseDouble(mN.txtNumber1.getText());
-		double b = Double.parseDouble(mN.txtNumber2.getText());
-		double c = Double.parseDouble(mN.txtNumber3.getText());
-		
+
+	public SecundaryClass(MainClass mn) { // recebe a tela como parâmetro ao invés de instanciar novamente
+		this.mN = mn;
+	}
+
+	public double CalculaMedia() {
+
+		double a = Double.parseDouble(mN.txtNumber1.getText().replaceAll(",", ".")); //replaceAll serve para alterar caracteres
+		double b = Double.parseDouble(mN.txtNumber2.getText().replaceAll(",", "."));
+		double c = Double.parseDouble(mN.txtNumber3.getText().replaceAll(",", "."));
+
 		return (a + b + c) / 3;
 	}
-	 
-	 public void ShowData() {
-		
-		 mN.txtMedia.setText(String.valueOf(CalculaMedia())); //Mostra o texto na tela
 
-		 double d = Double.parseDouble(mN.txtInputAverage.getText()); // transforma a txtbox de String para double
-		 if (CalculaMedia() >= d) {
-			 mN.txtSituacao.setText("Aluno aprovado!");
-		 } else {
-			 mN.txtSituacao.setText("Aluno reprovado!");
-		 }
-	 }
+	public void ShowData() {
+		mN.txtMedia = new JTextField();
+		mN.txtMedia.setEditable(false);
+		mN.txtMedia.setFont(new Font("Arial", Font.PLAIN, 15));
+		mN.txtMedia.setForeground(new Color(0, 0, 128));
+		mN.txtMedia.setBounds(124, 139, 211, 23);
+		mN.pane1.add(mN.txtMedia);
+		mN.txtMedia.setColumns(10);
+
+		mN.txtSituacao = new JTextField();
+		mN.txtSituacao.setEditable(false);
+		mN.txtSituacao.setForeground(new Color(0, 0, 128));
+		mN.txtSituacao.setFont(new Font("Arial", Font.PLAIN, 15));
+		mN.txtSituacao.setColumns(10);
+		mN.txtSituacao.setBounds(134, 173, 201, 23);
+		mN.pane1.add(mN.txtSituacao);
+
+		mN.txtMedia.setText(String.valueOf(CalculaMedia())); // Mostra o texto na tela
+
+		double d = Double.parseDouble(mN.txtInputAverage.getText().replaceAll(",", ".")); // transforma a txtbox de String para double
+		if (CalculaMedia() >= d) {
+			mN.txtSituacao.setText("Aluno aprovado!");
+		} else {
+			mN.txtSituacao.setText("Aluno reprovado!");
+		}
+	}
+
+	public void CleanCache() {
+		mN.txtNumber1.setText("");
+		mN.txtNumber2.setText("");
+		mN.txtNumber3.setText("");
+		
+		mN.txtSituacao.setText("");
+		mN.txtMedia.setText("");
+		mN.txtInputAverage.setText("");
+	}
 }
